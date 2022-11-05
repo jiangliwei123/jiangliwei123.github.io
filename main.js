8e2d61251b8b9d1f51347768bbdca6e6eede3f83
jiangliwei123 <117152382+jiangliwei123@users.noreply.github.com>
2022-11-05 15:42:46 +0800

    Add files via upload

--- main.js
+++ main.js
@@ -1,15 +1,23 @@
 function clickmuyu(){
     n++;
     document.getElementById("n").textContent=n;
-    var a = new Audio();
-    a.src="muyu2.mp3";
-    a.volume=0.3;
+    a.load();
     a.play();
-    document.getElementsByTagName("img")[0].setAttribute("width","390px");
+    document.getElementsByTagName("img")[0].setAttribute("width","387px");
     setTimeout(() => {
         document.getElementsByTagName("img")[0].setAttribute("width","400px");
     }, 50);
 }
 
+function tftip(){
+    document.getElementById("virtues").innerHTML='<span>click too fast!</span>';
+    document.getElementById("n").innerHTML=" ";
+    n-=3;
+    setTimeout(() => {
+        document.getElementById("virtues").innerHTML="<span>today's virtues:</span>";
+        document.getElementById("n").innerHTML=n;
+    }, 500);
+}
 var n = 0;
-
+a = document.getElementById("audio");
+a.volume=0.3;
