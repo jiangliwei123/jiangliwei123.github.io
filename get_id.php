7b8c798480fb1300ae3f9ae483f7429dd520fe57
jiangliwei123 <117152382+jiangliwei123@users.noreply.github.com>
2022-11-14 11:09:47 +0800

    Create get_id.php

---
+++ get_id.php
@@ -0,0 +1,25 @@
+<?php
+//声明CODE，获取小程序传过来的CODE
+$code = $_GET["code"];
+//配置appid
+$appid = "wx3ab87213bf4f7111";
+//配置appscret
+$secret = "cdf2490b120d2d31025ba82c38c678fc";
+//api接口
+$api = "https://api.weixin.qq.com/sns/jscode2session?appid={$appid}&secret={$secret}&js_code={$code}&grant_type=authorization_code";
+//获取GET请求
+function httpGet($url){
+    $curl = curl_init();
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($curl, CURLOPT_TIMEOUT, 500);
+    curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, true);
+    curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, true);
+    curl_setopt($curl, CURLOPT_URL, $url);
+    $res = curl_exec($curl);
+    curl_close($curl);
+    return $res;
+}
+//发送
+$str = httpGet($api);
+echo $str;
+?>
