b6c62d9a968dbda5fa537b98a3dfad53c040995d
jiangliwei123 <117152382+jiangliwei123@users.noreply.github.com>
2022-12-16 11:13:41 +0800

    Delete lovepage.js

--- lovepage.js
+++
@@ -1,8 +0,0 @@
-document.body.style.cursor = "url(https://tva1.sinaimg.cn/large/008vxvgGly1h9386kc3ywj305k05kt8j.jpg)";
-
-function getTimeDifference(){
-    var startDate = new Date("2021-12-25");
-    var currentDate = new Date();
-    var timeDifference = currentDate.getTime() - startDate.getTime();
-    var daysDifference = Math.floor(timeDifference / (1000 * 3600 * 24));
-    return daysDifference;}
